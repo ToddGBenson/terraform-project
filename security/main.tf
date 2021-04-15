412220ec8735bf7f8deb936f1a71d17b67cf449b
Todd Benson <todd.benson@smarsh.com>
2021-04-15 11:28:32 -0700

    Added network and VPCs

--- security/main.tf
+++ security/main.tf
@@ -14,9 +14,5 @@ module "security" {
 module "compute" {
   source          = "./compute"
   publicSubnetA   = module.network.publicSubnetA
-  appA            = module.network.appA
-  appB            = module.network.appB
-  appC            = module.network.appC
   BastionSG       = module.security.BastionSG
-  AppSG           = module.security.AppSG
 }
