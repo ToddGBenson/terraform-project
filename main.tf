e2a020591e8b2b0b95fe38d579ebb132f350e05d
Todd Benson <todd.benson@smarsh.com>
2021-05-11 10:49:09 -0700

    Added network and VPCs

--- main.tf
+++ main.tf
@@ -21,3 +21,17 @@ module "compute" {
   BastionSG       = module.security.BastionSG
   AppSG           = module.security.AppSG
 }
+
+module "containers" {
+  source          = "./containers"
+  ecsServiceRole  = module.security.ecsServiceRole
+  myLabKeyPair    = module.containers.myLabKeyPair
+  appA            = module.network.appA
+  appB            = module.network.appB
+  appC            = module.network.appC
+  EcsSG           = module.security.EcsSG
+  ecsExecutionRoleArn  = module.security.ecsExecutionRoleArn
+  ecsInstanceProfileId = module.security.EcsInstanceProfileId
+  KoffeeLuvTGArn  = module.network.KoffeeLuvTGArn
+  KoffeeLuvAlbName = module.network.KoffeeLuvAlbName
+}
